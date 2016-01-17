e258917edbca6e0be36febb2f2894935e5743331
TomL <lngrtm092@gmail.com>
2016-01-18 00:32:09 +0200

    Created temp json to wirk with temporarily until we start to use a real db

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,19 @@
+var tempJson = [
+    {
+        name: "Cyber Warfare",
+        desc: "Cyber Cyber Cyber Cyber Cyber Cyber Cyber Cyber Cyber",
+        price: 200
+    },
+    {
+        name: "Anatomy of Attack",
+        desc: "Zolantz Zolantz Zolantz Zolantz Zolantz Zolantz Zolantz",
+        price: 500
+    },
+    {
+        name: "Secure Java",
+        desc: "DorP DorP DorP DorP DorP DorP DorP DorP DorP DorP DorP",
+        price: 300
+    }
+]
+
+var marshalApp = angular.module('marshalApp', []);
