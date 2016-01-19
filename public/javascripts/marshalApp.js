ac0e6c4bb06e15c713ce873b468256772592a159
TomL <lngrtm092@gmail.com>
2016-01-20 01:19:29 +0200

    Added a login page (login.jade), added angular routing so we can make this app SPA, without refreshing the whole page

--- public/javascripts/marshalApp.js
+++ public/javascripts/marshalApp.js
@@ -1,23 +1,34 @@
-var tempJson = [
-    {
-        name: "Cyber Warfare",
-        desc: "Cyber Cyber Cyber Cyber Cyber Cyber Cyber Cyber Cyber",
-        price: 200
-    },
-    {
-        name: "Anatomy of Attack",
-        desc: "Zolantz Zolantz Zolantz Zolantz Zolantz Zolantz Zolantz",
-        price: 500
-    },
-    {
-        name: "Secure Java",
-        desc: "DorP DorP DorP DorP DorP DorP DorP DorP DorP DorP DorP",
-        price: 300
-    }
-]
+// var tempJson = [
+//     {
+//         name: "Cyber Warfare",
+//         desc: "Cyber Cyber Cyber Cyber Cyber Cyber Cyber Cyber Cyber",
+//         price: 200
+//     },
+//     {
+//         name: "Anatomy of Attack",
+//         desc: "Zolantz Zolantz Zolantz Zolantz Zolantz Zolantz Zolantz",
+//         price: 500
+//     },
+//     {
+//         name: "Secure Java",
+//         desc: "DorP DorP DorP DorP DorP DorP DorP DorP DorP DorP DorP",
+//         price: 300
+//     }
+// ]
 
-var marshalApp = angular.module('marshalApp', []);
+var marshalApp = angular.module('marshalApp', ['ngRoute']);
 
-marshalApp.controller('coursesCtrl', function($scope) {
-   $scope.courses = tempJson; 
+// marshalApp.controller('coursesCtrl', function($scope) {
+//    $scope.courses = tempJson; 
+// });
+
+marshalApp.controller('loginController', function($scope) {
+    console.log("A");
+});
+
+marshalApp.config(function($routeProvider) {
+   $routeProvider
+        .when('/', {
+            templateUrl : 'login'
+        });
 });
