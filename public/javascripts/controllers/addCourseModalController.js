5dfef86ef959a364a9f31be15a857d2d44aa7cdb
TomL <lngrtm092@gmail.com>
2016-02-05 00:32:30 +0200

    Adding course works only with button

--- public/javascripts/controllers/addCourseModalController.js
+++ public/javascripts/controllers/addCourseModalController.js
@@ -1,4 +1,15 @@
 angular.module('marshalApp')
-    .controller('addCourseModalCtrl', ['$scope', '$uibModalInstance', 'propGetter', function($scope, $uibModalInstance, propGetter) {
+    .controller('addCourseModalCtrl', ['$scope', '$uibModalInstance', 'propGetter', 'courseHandler', function($scope, $uibModalInstance, propGetter, courseHandler) {
         $scope.columns = propGetter.getPropNames();
+        $scope.addCourse = function() {
+            // Getting new course template from service
+            var newCourse = courseHandler.getCourseTemplate();
+            
+            // Putting each property we get from the inputs to the new object
+            $scope.columns.forEach(x => newCourse[x.realName] = x.value);
+            
+            // Pushes the new course to the course list and closes the modal
+            courseHandler.addCourse(newCourse);
+            $uibModalInstance.close();
+        }
     }]);
---
+++ public/javascripts/services/courseHandler.js
@@ -0,0 +1,66 @@
+angular.module('marshalApp')
+    .service('courseHandler', [CourseHandler])
+
+function CourseHandler() {
+    var courses = [
+        {
+            name: "Design Patterns",
+            symbol: "6813",
+            desc: "ההשתלמות מלמדת טכניקות Design ע\"פ תבניות קבועות ידועות מראש, כולל דרכי השימוש בהם בקוד והרצאת העשרה בנושא REST.",
+            syl: "מבוא ל-Design Patterns<br>Singleton<br>Iterator<br>Framework<br>Factory<br>Template<br>MVC<br>Observer<br>Decorator",
+            duration: "5 ימים",
+            reqs: "לחייל יש מקצוע תכניתן או השכלת אקדמאי מוסמך וגם בעל סיווג שמור, בעל פז\"מ שנה, יתרת שירות חצי שנה ועשה מבחן כניסה על OOP.",
+            destPop: "תכניתנים ואקדמאים בוגרי מדעי המחשב/הנדסת תכונה/הנדסת מערכות מידע",
+            dep: "עוצמה",
+            price: 1455,
+            notes: "יש לבצע מבחן כניסה להשתלמות"
+        },
+        {
+            name: "פיתוח Front End",
+            symbol: "2633",
+            desc: "עקרונות בניית אפליקציות WEB דינאמיות תוך שימוש ביכולות השונות של השפות HTML וJavascript וספריות JQuery וBootstrap.",
+            syl: "מבוא ל-HTML<br>Forms<br>HTML5 Forms<br>מבוא ל-Javascript<br>ECMAScript6",
+            duration: "5 ימים",
+            reqs: "לחייל יש מקצוע כותב לומדה, נתמ\"מ, מגן בסייבר או תכניתן",
+            destPop: "תכניתנים, מגן בסייבר, כותב לומדה, נתמ\"מ",
+            dep: "היל\"ה",
+            price: 993,
+            notes: ""
+        },
+        {
+            name: "Design Patterns",
+            symbol: "6813",
+            desc: "ההשתלמות מלמדת טכניקות Design ע\"פ תבניות קבועות ידועות מראש, כולל דרכי השימוש בהם בקוד והרצאת העשרה בנושא REST.",
+            syl: "מבוא ל-Design Patterns<br>Singleton<br>Iterator<br>Framework<br>Factory<br>Template<br>MVC<br>Observer<br>Decorator",
+            duration: "5 ימים",
+            reqs: "לחייל יש מקצוע תכניתן או השכלת אקדמאי מוסמך וגם בעל סיווג שמור, בעל פז\"מ שנה, יתרת שירות חצי שנה ועשה מבחן כניסה על OOP.",
+            destPop: "תכניתנים ואקדמאים בוגרי מדעי המחשב/הנדסת תכונה/הנדסת מערכות מידע",
+            dep: "עוצמה",
+            price: 1455,
+            notes: "יש לבצע מבחן כניסה להשתלמות"
+        }
+    ];
+    
+    this.getCourses = function() {
+        return courses;
+    };
+    
+    this.addCourse = function(course) {
+        courses.push(course);
+    };
+    
+    this.getCourseTemplate = function() {
+        return {
+            name: "",
+            symbol: "",
+            desc: "",
+            syl: "",
+            duration: "",
+            reqs: "",
+            destPop: "",
+            dep: "",
+            price: 0,
+            notes: ""
+        }
+    };
+}
