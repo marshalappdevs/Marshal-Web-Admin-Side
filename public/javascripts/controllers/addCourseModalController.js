90a62c5924394f800f761a9e4a1c9e47196b6975
TomL94 <lngrtm092@gmail.com>
2016-02-06 12:56:30 +0200

    Added option to submit a new course using the Enter(13) button

--- public/javascripts/controllers/addCourseModalController.js
+++ public/javascripts/controllers/addCourseModalController.js
@@ -1,15 +1,23 @@
 angular.module('marshalApp')
     .controller('addCourseModalCtrl', ['$scope', '$uibModalInstance', 'propGetter', 'courseHandler', function($scope, $uibModalInstance, propGetter, courseHandler) {
         $scope.columns = propGetter.getPropNames();
-        $scope.addCourse = function() {
-            // Getting new course template from service
-            var newCourse = courseHandler.getCourseTemplate();
-            
-            // Putting each property we get from the inputs to the new object
-            $scope.columns.forEach(x => newCourse[x.realName] = x.value);
-            
-            // Pushes the new course to the course list and closes the modal
-            courseHandler.addCourse(newCourse);
-            $uibModalInstance.close();
+        $scope.addCourse = function(event) {
+            if (event && (event.keyCode === 13 || event.target.id === 'addButton')) {
+                // Getting new course template from service
+                var newCourse = courseHandler.getCourseTemplate();
+                
+                // Putting each property we get from the inputs to the new object
+                $scope.columns.forEach(x => newCourse[x.realName] = x.value);
+                
+                // Pushes the new course to the course list and closes the modal
+                courseHandler.addCourse(newCourse);
+                $uibModalInstance.close();
+                
+                $scope.clearInputValues();
+            }
+        }
+        
+        $scope.clearInputValues = function() {
+            $scope.columns.forEach(x => x.value = "");
         }
     }]);
