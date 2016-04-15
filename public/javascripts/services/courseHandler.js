2fbad3057653e518a5ba37739f396131d8bf1203
TomL94 <lngrtm092@gmail.com>
2016-04-16 01:32:05 +0300

    UI now getting information from the DB

--- public/javascripts/services/courseHandler.js
+++ public/javascripts/services/courseHandler.js
@@ -1,36 +1,41 @@
 angular.module('marshalApp')
-    .service('courseHandler', [CourseHandler]);
+    .service('courseHandler', ['$http', CourseHandler]);
 
-function CourseHandler() {
-    var courses = [
-        {
-            ID: 1,
-            Name: "Design Patterns",
-            CourseCode: "6813",
-            Description: "ההשתלמות מלמדת טכניקות Design ע\"פ תבניות קבועות ידועות מראש, כולל דרכי השימוש בהם בקוד והרצאת העשרה בנושא REST.",
-            Syllabus: "מבוא ל-Design Patterns<br>Singleton<br>Iterator<br>Framework<br>Factory<br>Template<br>MVC<br>Observer<br>Decorator",
-            DurationInDays: "5 ימים",
-            TargetPopulation: "תכניתנים ואקדמאים בוגרי מדעי המחשב/הנדסת תכונה/הנדסת מערכות מידע",
-            ProfessionalDomain: "עוצמה",
-            Price: 1455,
-            Comments: "יש לבצע מבחן כניסה להשתלמות"
-        },
-        {
-            ID: 3,
-            Name: "פיתוח Front End",
-            CourseCode: "2633",
-            Description: "עקרונות בניית אפליקציות WEB דינאמיות תוך שימוש ביכולות השונות של השפות HTML וJavascript וספריות JQuery וBootstrap.",
-            Syllabus: "מבוא ל-HTML<br>Forms<br>HTML5 Forms<br>מבוא ל-Javascript<br>ECMAScript6",
-            DurationInDays: "5 ימים",
-            TargetPopulation: "תכניתנים, מגן בסייבר, כותב לומדה, נתמ\"מ",
-            ProfessionalDomain: "היל\"ה",
-            Price: 993,
-            Comments: "",
-            PictureUrl: "https://rogteran.files.wordpress.com/2015/06/js.png"
-        }
-    ];
+function CourseHandler($http) {
+     var courses = [ ];
+    //     {
+    //         ID: 1,
+    //         Name: "Design Patterns",
+    //         CourseCode: "6813",
+    //         Description: "ההשתלמות מלמדת טכניקות Design ע\"פ תבניות קבועות ידועות מראש, כולל דרכי השימוש בהם בקוד והרצאת העשרה בנושא REST.",
+    //         Syllabus: "מבוא ל-Design Patterns<br>Singleton<br>Iterator<br>Framework<br>Factory<br>Template<br>MVC<br>Observer<br>Decorator",
+    //         DurationInDays: "5 ימים",
+    //         TargetPopulation: "תכניתנים ואקדמאים בוגרי מדעי המחשב/הנדסת תכונה/הנדסת מערכות מידע",
+    //         ProfessionalDomain: "עוצמה",
+    //         Price: 1455,
+    //         Comments: "יש לבצע מבחן כניסה להשתלמות"
+    //     },
+    //     {
+    //         ID: 3,
+    //         Name: "פיתוח Front End",
+    //         CourseCode: "2633",
+    //         Description: "עקרונות בניית אפליקציות WEB דינאמיות תוך שימוש ביכולות השונות של השפות HTML וJavascript וספריות JQuery וBootstrap.",
+    //         Syllabus: "מבוא ל-HTML<br>Forms<br>HTML5 Forms<br>מבוא ל-Javascript<br>ECMAScript6",
+    //         DurationInDays: "5 ימים",
+    //         TargetPopulation: "תכניתנים, מגן בסייבר, כותב לומדה, נתמ\"מ",
+    //         ProfessionalDomain: "היל\"ה",
+    //         Price: 993,
+    //         Comments: "",
+    //         PictureUrl: "https://rogteran.files.wordpress.com/2015/06/js.png"
+    //     }
+    // ];
     
     this.getCourses = function() {
+        $http.get('api/courses')
+        .then(function (results) {
+            results.data.forEach(x => courses.push(x));
+        })
+        
         return courses;
     };
     
