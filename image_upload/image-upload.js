422ebc36d37cbed1bf0669f492a8f585130296f4
TomL94 <lngrtm092@gmail.com>
2016-04-19 00:21:19 +0300

    Changed the pathes to the image folder

--- image_upload/image-upload.js
+++ image_upload/image-upload.js
@@ -2,7 +2,7 @@ var multer = require('multer');
 
 var storage = multer.diskStorage({ //multers disk storage settings
     destination: function (req, file, cb) {
-        cb(null, 'public/images/')
+        cb(null, path.join(__dirname, '../public/images/'))
     },
     filename: function (req, file, cb) {
         var datetimestamp = Date.now();
