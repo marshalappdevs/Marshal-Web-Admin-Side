175a058aa1b9d24e14b9205ba5694703b2bda90a
Itamar <doriitamar@gmail.com>
2016-07-04 22:33:20 +0300

    Fixed courses API
    And all courses API work with a single connection! Wooho!

--- routes/index.js
+++ routes/index.js
@@ -95,75 +95,66 @@ router.post('/api/courses', function(req, res) {
 });
 
 // // Update courses (any property)
-// router.put('/api/courses', function(req, res) {
-//     db
-//     .then(function (courses) {
-//         courses.update({ ID: req.body.ID}, req.body, function(err, result) {
-//             // If everything's alright
-//             if (!err && result.ok === 1) {
-//                 setLastUpdateNow();
-//                 res.json({ code: 200});
-//             } else {
-//                 res.json({error: "something went wrong.."});
-//                 console.log(err, result);
-//             }
-//         });
-//     });
-// });
+router.put('/api/courses/:id', function(req, res) {
+    courses.update({ ID: req.params.id}, req.body, function(err, result) {
+            // If everything's alright
+            if (!err && result.ok === 1) {
+                setLastUpdateNow();
+                res.json({ code: 200});
+            } else {
+                res.json({code:400, error: "something went wrong.."});
+                console.log(err, result);
+            }
+    });
+});
 
 // // Delete courses
-// router.delete('/api/courses/:courseId', function(req, res) {
-//     db
-//     .then(function(courses) {
-//         courses.remove({ ID: req.params.courseId }, function(err, result) {
-//             if (!err) {
-//                 console.log(result);
-//                 setLastUpdateNow();
-//                 res.json({ code: 201, message: "Deleted course!" });
-//             } else {
-//                 console.log(err);
-//                 res.json({ code: 400, message: "Couldn't delete course" })
-//             }
-//         });
-//     });
-// });
+router.delete('/api/courses/:courseId', function(req, res) {
+    courses.remove({ ID: req.params.courseId }, function(err, result) {
+        if (!err) {
+            console.log(result);
+            setLastUpdateNow();
+            res.json({ code: 201, message: "Deleted course!" });
+        } else {
+            console.log(err);
+            res.json({ code: 400, message: "Couldn't delete course" })
+        }
+    });
+});
 
 // // Images
 
-// router.get('/api/images/:courseId', function (req, res, next) {
-//     db
-//     .then(function (courses) {
-//         courses.findOne({ CourseCode: req.params.courseId }, 'PictureUrl', function(err, picUrl) {
-//             // If there's no error
-//             if (!err) {
-//                 res.sendFile('images/' + picUrl._doc.PictureUrl, { root: path.join(__dirname, '../public') });
-//             }
-//         })
-//     });
-// });
+router.get('/api/images/:courseId', function (req, res, next) {
+    courses.findOne({ CourseCode: req.params.courseId }, 'PictureUrl', function(err, picUrl) {
+        // If there's no error
+        if (!err) {
+            res.sendFile('images/' + picUrl._doc.PictureUrl, { root: path.join(__dirname, '../public') });
+        }
+    })
+});
 
-// router.post('/api/images', function(req, res) {
-//     // Checks if only url has been sent
-//     if (req.body.imageUrl) {
-//         var datetimestamp = Date.now();
-//         var fileName = "file-" + datetimestamp + "." + req.body.imageUrl.split('.')[req.body.imageUrl.split('.').length - 1];
-//         var file = fs.createWriteStream(path.join(__dirname, '../public/images/') + fileName);
-//         https.get(req.body.imageUrl, function(result) {
-//             result.pipe(file);
-//             setLastUpdateNow();
-//             res.json({ error_code : 0, err_desc : null, filename : fileName });
-//         });
-//     } else {
-//         // If not only url, then the user want to upload from his local disk
-//         upload(req, res, function(err) {
-//             if (err) {
-//                 res.json({ error_code : 1, err_desc : err });
-//                 return;
-//             }
-//             res.json({ error_code : 0, err_desc : null, filename : req.file.filename });
-//         });
-//     }
-// });
+router.post('/api/images', function(req, res) {
+    // Checks if only url has been sent
+    if (req.body.imageUrl) {
+        var datetimestamp = Date.now();
+        var fileName = "file-" + datetimestamp + "." + req.body.imageUrl.split('.')[req.body.imageUrl.split('.').length - 1];
+        var file = fs.createWriteStream(path.join(__dirname, '../public/images/') + fileName);
+        https.get(req.body.imageUrl, function(result) {
+            result.pipe(file);
+            setLastUpdateNow();
+            res.json({ error_code : 0, err_desc : null, filename : fileName });
+        });
+    } else {
+        // If not only url, then the user want to upload from his local disk
+        upload(req, res, function(err) {
+            if (err) {
+                res.json({ error_code : 1, err_desc : err });
+                return;
+            }
+            res.json({ error_code : 0, err_desc : null, filename : req.file.filename });
+        });
+    }
+});
 
 // /////////////////////////////// IDO //////////////////////////////
 
