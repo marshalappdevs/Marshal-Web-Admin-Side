d0d396be1b569901a65df9375b3daa13ae05fc8d
Itamar <doriitamar@gmail.com>
2016-07-06 08:02:09 +0300

    Started making angular infrastructure, routing and template are shitty

--- public/javascripts/appModule.js
+++ public/javascripts/appModule.js
@@ -1,11 +1,29 @@
-angular.module
+var app = angular.module
 (
     'marshalApp',
     [
-        'ngRoute',
-        'ngAnimate',
-        'ui.bootstrap',
-        'ngSanitize',
-        'ngFileUpload'
+        'ngRoute'
     ]
 );
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            template: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        })
+        .when('/courses', {
+            templateUrl : './templates/home.html'
+        })
+        .when('/malshabs', {
+            templateUrl: './templates/malshabs.html'
+        })
+        .when('/push', {
+            templateUrl: './templates/push.html'
+        })
+        .when('/materials', {
+            templateUrl: './templates/materials.html'
+        })
+        .when('/meetups', {
+            templateUrl: './templates/meetups.html'
+        });
+});
