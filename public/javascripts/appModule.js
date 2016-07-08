775a09b00927c2e7a1bdd74ff101b276136d9ae0
Itamar <doriitamar@gmail.com>
2016-07-08 10:45:38 +0300

    Made angular routing!

--- public/javascripts/appModule.js
+++ public/javascripts/appModule.js
@@ -1,9 +1,10 @@
-var app = angular.module('marshalApp', ['ngMaterial', 'ngMdIcons']);
+var app = angular.module('marshalApp', ['ngMaterial', 'ngMdIcons', 'ngRoute']);
 
 app.controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog', function($scope, $mdBottomSheet, $mdSidenav, $mdDialog){
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
+
  	$scope.menu = [
     {
       link : '',
@@ -33,89 +34,7 @@ app.controller('AppCtrl', ['$scope', '$mdBottomSheet','$mdSidenav', '$mdDialog',
       icon: 'settings'
     }
   ];
-  $scope.activity = [
-      {
-        what: 'לתכנת את הדרעק הזה',
-        who: 'איתמר',
-        when: '3:08PM',
-        notes: " לא נוגע בצד לקוח יותר בחיים!"
-      },
-      {
-         what: 'לתכנת את הדרעק הזה',
-        who: 'איתמר',
-        when: '3:08PM',
-        notes: " לא נוגע בצד לקוח יותר בחיים!"
-      },
-      {
-       what: 'לתכנת את הדרעק הזה',
-        who: 'איתמר',
-        when: '3:08PM',
-        notes: " לא נוגע בצד לקוח יותר בחיים!"
-      },
-      {
-         what: 'לתכנת את הדרעק הזה',
-        who: 'איתמר',
-        when: '3:08PM',
-        notes: " לא נוגע בצד לקוח יותר בחיים!"
-      },
-      {
-           what: 'לתכנת את הדרעק הזה',
-        who: 'איתמר',
-        when: '3:08PM',
-        notes: " לא נוגע בצד לקוח יותר בחיים!"
-      },
-    ];
   $scope.alert = ''; }]);
-//   $scope.showListBottomSheet = function($event) {
-//     $scope.alert = '';
-//     $mdBottomSheet.show({
-//       template: '<md-bottom-sheet class="md-list md-has-header"> <md-subheader>Settings</md-subheader> <md-list> <md-item ng-repeat="item in items"><md-item-content md-ink-ripple flex class="inset"> <a flex aria-label="{{item.name}}" ng-click="listItemClick($index)"> <span class="md-inline-list-icon-label">{{ item.name }}</span> </a></md-item-content> </md-item> </md-list></md-bottom-sheet>',
-//       controller: 'ListBottomSheetCtrl',
-//       targetEvent: $event
-//     }).then(function(clickedItem) {
-//       $scope.alert = clickedItem.name + ' clicked!';
-//     });
-//   };
-  
-//   $scope.showAdd = function(ev) {
-//     $mdDialog.show({
-//       controller: DialogController,
-//       template: '<md-dialog aria-label="Mango (Fruit)"> <md-content class="md-padding"> <form name="userForm"> <div layout layout-sm="column"> <md-input-container flex> <label>First Name</label> <input ng-model="user.firstName" placeholder="Placeholder text"> </md-input-container> <md-input-container flex> <label>Last Name</label> <input ng-model="theMax"> </md-input-container> </div> <md-input-container flex> <label>Address</label> <input ng-model="user.address"> </md-input-container> <div layout layout-sm="column"> <md-input-container flex> <label>City</label> <input ng-model="user.city"> </md-input-container> <md-input-container flex> <label>State</label> <input ng-model="user.state"> </md-input-container> <md-input-container flex> <label>Postal Code</label> <input ng-model="user.postalCode"> </md-input-container> </div> <md-input-container flex> <label>Biography</label> <textarea ng-model="user.biography" columns="1" md-maxlength="150"></textarea> </md-input-container> </form> </md-content> <div class="md-actions" layout="row"> <span flex></span> <md-button ng-click="answer(\'not useful\')"> Cancel </md-button> <md-button ng-click="answer(\'useful\')" class="md-primary"> Save </md-button> </div></md-dialog>',
-//       targetEvent: ev,
-//     })
-//     .then(function(answer) {
-//       $scope.alert = 'You said the information was "' + answer + '".';
-//     }, function() {
-//       $scope.alert = 'You cancelled the dialog.';
-//     });
-//   };
-// }]);
-
-// app.controller('ListBottomSheetCtrl', function($scope, $mdBottomSheet) {
-//   $scope.items = [
-//     { name: 'Share', icon: 'share' },
-//     { name: 'Upload', icon: 'upload' },
-//     { name: 'Copy', icon: 'copy' },
-//     { name: 'Print this page', icon: 'print' },
-//   ];
-  
-//   $scope.listItemClick = function($index) {
-//     var clickedItem = $scope.items[$index];
-//     $mdBottomSheet.hide(clickedItem);
-//   };
-// });
-
-// function DialogController($scope, $mdDialog) {
-//   $scope.hide = function() {
-//     $mdDialog.hide();
-//   };
-//   $scope.cancel = function() {
-//     $mdDialog.cancel();
-//   };
-//   $scope.answer = function(answer) {
-//     $mdDialog.hide(answer);
-//   };
-// };
 
 app.directive('userAvatar', function() {
   return {
@@ -124,7 +43,7 @@ app.directive('userAvatar', function() {
   };
 });
 
-app.config(function($mdThemingProvider) {
+app.config(function($mdThemingProvider, $routeProvider) {
   var customBlueMap = 		$mdThemingProvider.extendPalette('light-blue', {
     'contrastDefaultColor': 'light',
     'contrastDarkColors': ['50'],
@@ -138,6 +57,19 @@ app.config(function($mdThemingProvider) {
     })
     .accentPalette('red');
   $mdThemingProvider.theme('input', 'default')
-        .primaryPalette('grey')
+        .primaryPalette('grey');
+
+  $routeProvider.
+      when('/', {
+        templateUrl: 'javascripts/example.html',
+        controller: 'ExampleCtrl'
+      }).
+      when('/example', {
+        templateUrl: 'templates/example2.html',
+        controller: 'ExampleTwoCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
 });
 
