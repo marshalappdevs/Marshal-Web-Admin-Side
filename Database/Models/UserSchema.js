7b2ac9b257a2d9948b00a9833ce7deabe033c88f
Itamar <doriitamar@gmail.com>
2016-07-13 19:11:19 +0300

    Creating users schema

---
+++ Database/Models/UserSchema.js
@@ -0,0 +1,48 @@
+var mongoose = require('mongoose');
+var sha = require('sha.js');
+var sha256 = createHash('sha256');
+
+// User schema
+var UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['Client', 'Admin'],
+        default: 'Client'
+    }
+});
+
+// Hash user password
+UserSchema.pre('save', function(next){
+    var user = this;
+    if(this.isModified('password') || this.isNew) {
+        this.password = sha256.update(this.password, 'utf8').digest('hex');
+        next();
+    }
+    else
+    {
+        return next();
+    }
+});
+
+UserSchema.methods.comparePass = function(pw, cb) {
+    pw = sha256.update(this.password, 'utf8').digest('hex');
+
+    if(pw == this.password)
+    {
+        cb(null, true);
+    }
+    else {
+        cb(null, false);
+    }
+}
+
+module.exports = mongoose.model('User', UserSchema);
