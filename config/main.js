709411a72fb3bd48316a0adfa5708d6ff5a094cc
Itamar <doriitamar@gmail.com>
2016-08-12 13:37:36 +0300

    Removed keys

--- config/main.js
+++ config/main.js
@@ -1,6 +1,6 @@
 module.exports = {
-    'secret': 'HahdjHDFY387RfhWSNVJN8gywW8VHo8wyv9',
-    'loginSecret': 'HaUGD366fgdkdsf7d7HDuicnuiNC8Yc8XJm9',
+    'secret': 'secret',
+    'loginSecret': 'secret',
     'database': 'mongodb://marshalmongo.cloudapp.net/Marshal',
-    'expectedStringPrefix': "marshaldevapps"
+    'expectedStringPrefix': "secret"
 };
