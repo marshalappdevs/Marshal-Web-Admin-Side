6e3a0e027bde5fa4d812fa1573658e6dbb9afbdf
Itamar Dori <doriitamar@gmail.com>
2016-08-12 17:20:56 +0300

    For db auth

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ require('../config/passportAdmin')(passport);
 require('../config/passportLogin')(passport);
 
 // DB connection
-mongoose.connect('mongodb://marshalmongo.cloudapp.net/Marshal');
+mongoose.connect('mongodb://marshalmongo.cloudapp.net/Marshal', {user: config.dbUser, pass: config.dbPass});
 
 function setLastUpdateNow() {
     console.log('setLastUpdateNow');
@@ -489,7 +489,7 @@ router.post('/api/gcm/sendpush/:messageContent', function(req, res) {
             return console.error(err);
         else if (registerations.length > 0) {
                 // Set up the sender with marshaldevs@gmail.com API key
-            var sender = new gcm.Sender('AIzaSyAkVAxVLkoV-sJ5jzPa2BfKZnvK5ASGOlk');
+            var sender = new gcm.Sender(config.serverApi);
 
             // Initialize Message object
             var message = new gcm.Message();
